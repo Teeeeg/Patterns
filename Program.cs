068432c93208bec61c464772f6d60ee23f06f7f6
Teeeeg <iitaoyupeng@icloud.com>
2022-07-13 09:59:51 +0800

    Factory and Prototype

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Patterns.AbstractFactory;
+using Patterns.Prototype;
 
+namespace Patterns;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee john = new Employee();
+        john.names = new string[] { "John", "Smith" };
+        john.address = new Address("London Road", 123);
+        john.salary = 123000;
+
+
+        var jane = john.DeepCopy();
+        jane.address.houseNumber = 321;
+        Console.WriteLine(john);
+        Console.WriteLine(jane);
+    }
+}
